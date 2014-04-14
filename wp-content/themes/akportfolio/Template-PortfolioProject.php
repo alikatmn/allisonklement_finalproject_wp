0f14fd5b280ca9f643e8217e845a92eb6fa81194
alikatmn <allisonklement@allisons-iMac.local>
2014-04-14 00:16:06 -0500

    styling inner page
    worked on css, added fonts and flex slider to function, tried to do the
    min.'s

--- wp-content/themes/akportfolio/Template-PortfolioProject.php
+++ wp-content/themes/akportfolio/Template-PortfolioProject.php
@@ -4,74 +4,87 @@
 */
 
 get_header(); ?>
+
  
 	<div id="primary">
 		<div id="content" role="main">
+			<div class='section'>
+				<div class='page-border'>
+				<div class='portfolio-page'>
  
 			<?php while ( have_posts() ) : the_post(); ?>
  
 				<h1><?php the_title(); ?></h1>
 				<?php the_content(); ?>
  
+ 			<!--<div class="project-top">-->
+				
 				<?php 
  
 				// check for rows (parent repeater)
 				if( have_rows('project_header') ): ?>
-					<div id="project-header">
-					<?php 
- 
-					// loop through rows (parent repeater)
-					while( have_rows('project_header') ): the_row(); ?>
-						<div>
-
-							<h3><?php the_sub_field('section_name'); ?></h3>
-							
-
-<?php 
-$images = get_sub_field('section_image_gallery');
-
-if( $images ): ?>
-    <div id="slider" class="flexslider">
-        <ul class="slides">
-            <?php foreach( $images as $image ): ?>
-                <li>
-                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                    <p><?php echo $image['caption']; ?></p>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-<?php endif; ?>
-
-
+			<section class='project-header'>
+								
 							<?php 
  
-							// check for rows (sub repeater)
-							if( have_rows('section_list') ): ?>
-								<ul>
-								<?php 
+							// loop through rows (parent repeater)
+							while( have_rows('project_header') ): the_row(); ?>
+								<section class='section-name'>
+									<h1><?php the_sub_field('section_name'); ?></h1>
+								</section>
+
+								<section class='top-background-img'>
+									<img src="<?php the_sub_field('section_background_image'); ?>" >
+								</section>
+
+				<?php 
  
-								// loop through rows (sub repeater)
-								while( have_rows('section_list') ): the_row();
+				// check for rows (sub repeater)
+				if( have_rows('section_list') ): ?>
+					<section class="section-list">
+						<ul>
+							<?php 
  
-									// display each item as a list 
-									?>
-									<li <?php if( get_sub_field('submenu') ) ?>><?php the_sub_field('submenu'); ?></li>
+							// loop through rows (sub repeater)
+							while( have_rows('section_list') ): the_row();
+ 
+							// display each item as a list 
+							?>
+							<li class="submenu" <?php if( get_sub_field('submenu') ) ?>><?php the_sub_field('submenu'); ?></li>
+
+								
+							<?php endwhile; ?>
+						</ul>
+						</section><!-- /section_list -->
+							<?php endif; //if( get_sub_field('section_list') ): ?>
+					
+			</section><!-- /project header -->
 
 				
 
+							<?php 
+							$images = get_sub_field('section_image_gallery');
 
+							if( $images ): ?>
+				    <div id="slider" class="flexslider">
+				    <section class='project-gallery'>
+				        <ul class="slides">
+				            <?php foreach( $images as $image ): ?>
+				                <li class='indv-slide'>
+				                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+				                    <p><?php echo $image['caption']; ?></p>
+				                </li>
+				            <?php endforeach; ?>
+				        </ul>
+				    </div> <!-- #slider -->
+							<?php endif; ?>
+					</section> <!-- /project gallery -->
 
-
-								<?php endwhile; ?>
-								</ul>
-							<?php endif; //if( get_sub_field('section_list') ): ?>
-
-									<?php 
-
- 
+							<?php 
 							// check for rows (sub repeater)
 							if( have_rows('list_item') ): ?>
+							<div class='challenges'>
+								<section class='list-items'>
 								<ul>
 								<?php 
  
@@ -80,23 +93,27 @@ if( $images ): ?>
  
 									// display each item as a list 
 									?>
-									<li <?php if( get_sub_field('list_item_headline') ) ?>><?php the_sub_field('list_item_headline'); ?></li>
-									<li <?php if( get_sub_field('list_item_paragraph') ) ?>><?php the_sub_field('list_item_paragraph'); ?></li>
+									<div class='list-headline-bkg'>
+										<li class='list-headline' <?php if( get_sub_field('list_item_headline') ) ?>><?php the_sub_field('list_item_headline'); ?></li>
+									</div>
+									<div class='list-paragraph-bkg'>
+									<li class='list-paragraph' <?php if( get_sub_field('list_item_paragraph') ) ?>><?php the_sub_field('list_item_paragraph'); ?></li>
+									</div>
 
-
-
-											<?php endwhile; ?>
+								<?php endwhile; ?>
 								</ul>
+								</section> <!-- /list-items -->
+							</div><!-- /challenges -->
 							<?php endif; //if( get_sub_field('list_item') ): ?>
 
-
-						</div>	
- 
 					<?php endwhile; // while( has_sub_field('project_header') ): ?>
-					</div>
+					
 				<?php endif; // if( get_field('project_header') ): ?>
  
 			<?php endwhile; // end of the loop. ?>
- 
+						<!--</div></project top -->
+ 		</div>
+ 	</div>
+ 	</div>
 		</div><!-- #content -->
 	</div><!-- #primary -->
--- wp-content/themes/akportfolio/functions.php
+++ wp-content/themes/akportfolio/functions.php
@@ -93,9 +93,13 @@ function creataliportfolio_scripts() {
 
 	//wp_enqueue_script( 'creataliportfolio-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'Lato', 'http://fonts.googleapis.com/css?family=Lato:400,700,300', false, false, false );
+	//wp_enqueue_script( 'Lato', 'http://fonts.googleapis.com/css?family=Lato:400,700,300', false, false, false );
+	wp_enqueue_script( 'jquery');
+	wp_enqueue_script( 'flexslider', get_template_directory_uri() . '/js/flexslider.js', array(), '20130115', true );
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array(), '20130115', true );
 
 	wp_enqueue_script( 'creataliportfolio-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
